febbf98244c2804cafee52838843a31058f0a9fd
kkdnl007 <kkdnl2888@gmail.com>
2024-10-23 15:22:30 +0800

    Update kuake.js

--- kuake.js
+++ kuake.js
@@ -8,12 +8,6 @@
 
 **************************************
 
-[rewrite_local]
-^https?:\/\/drive.*\.quark\.cn\/.+\/clouddrive\/(member.+|distribute\/detail.+|capacity\/growth\/info) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/kuake.js
-
-[mitm]
-hostname = drive*.quark.cn
-
 *************************************/
 
 
